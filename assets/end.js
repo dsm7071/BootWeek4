0b7ea4cae60b7489cc557ad34caccb37973c4bd0
dsm7071 <107210806+dsm7071@users.noreply.github.com>
2022-08-06 13:57:32 -0500

    minor syntax changes to paths

--- assets/end.js
+++ assets/end.js
@@ -1,3 +1,5 @@
+//This script will sqave the score and take you to the main page
+
 const username = document.querySelector("#username")
 const saveScoreBtn = document.querySelector("#saveScoreBtn")
 const finalScore = document.querySelector("#finalScore")
